3ff4a7da2e491e4e9c1bee93ef5cc57e0f59b892
bellamysmith <bellamysmith1@gmail.com>
2014-08-21 16:10:56 -0700

    added board structure and div repeats

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+var app = angular.module('TicTac', []);
+app.controller('TicTacControl', function($scope) {
+	$scope.gameBoard = [['','',''],['','',''],['','','']];
 
+	$scope.takeOver = function(x,y,symbol) {
+		$scope.gameBoard[x][y] = symbol;
+		console.log($scope.gameBoard);
+	}
+	console.log('IM WORKING')
+
+});
