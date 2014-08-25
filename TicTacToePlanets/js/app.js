6016ff4af2e6dd9dc4f13c2a5234441649daef69
bellamysmith <bellamysmith1@gmail.com>
2014-08-25 14:51:55 -0700

    Updating before angular fire

--- TicTacToePlanets/js/app.js
+++ TicTacToePlanets/js/app.js
@@ -45,8 +45,8 @@ $scope.token = function(position) {
     $scope.checkForTie();
     $scope.checkForWin();
 
-console.log($scope.checkForTie());
-console.log('win: ' + $scope.checkForWin());
+//console.log($scope.checkForTie());
+//console.log('win: ' + $scope.checkForWin());
 
     }
 
@@ -64,7 +64,7 @@ console.log('win: ' + $scope.checkForWin());
       $scope.userToken -= 1;
      
       }
-    console.log($scope.userToken)
+    //console.log($scope.userToken)
     
 };
 
@@ -92,16 +92,60 @@ $scope.checkForTie = function() {
   }
 };
 
-/*$scope.checkForWin = function(i) {
-  if ($scope.cells.slice(0,3).toString() === "X,X,X"){
+
+$scope.checkForWin = function() {
+  $scope.row1 = $scope.cells.slice(0,3);
+  $scope.row2 = $scope.cells.slice(3,6); 
+  $scope.row3 = $scope.cells.slice(6,9); 
+  $scope.column1 = [$scope.row1[0],$scope.row2[0], $scope.row3[0]];
+  $scope.column2 = [$scope.row1[1], $scope.row2[1], $scope.row3[1]];
+  $scope.column3 = [$scope.row1[2], $scope.row2[2], $scope.row3[2]];
+  $scope.diag1 = [$scope.row1[0], $scope.row2[1], $scope.row3[2]];
+  $scope.diag2 = [$scope.row1[2], $scope.row2[1], $scope.row3[0]];
+
+  $scope.winningConditions = [$scope.row1.toString(), $scope.row2.toString(), $scope.row3.toString(), $scope.column1.toString(), $scope.column2.toString(), $scope.column3.toString(), $scope.diag1.toString(), $scope.diag2.toString()];
+
+  for (i = 0; i < $scope.winningConditions.length; i ++) {
+    if ($scope.winningConditions[i] === 'X,X,X') {
+      return true;
+    } else if ($scope.winningConditions[i] === 'O,O,O') {
+      return true;
+    }
+  }
+
+}
+
+$scope.reset = function() {
+  $scope.cells = ["", "", "", "", "", "", "", "", ""];
+  $scope.userToken = 1;
+}
+
+
+
+
+
+  /*if ($scope.row1 === 'X,X,X' || $scope.row2 === 'X,X,X' || $scope.row3 === 'X,X,X') {
     return true;
-  } else if {
+  } else if ($scope.row1 === 'O,O,O' || $scope.row2 === 'O,O,O' || $scope.row3 === 'O,O,O') {
+    return true;
+  } else if (for (i = 0; 1 < 9; i ++) {
+    $scope.col1[] = ($scope.cells[(i % 3 == 0)]) === 'X') {
+    return true;
+  }
+  } 
+}*/
+
+
+  /*if ($scope.row1 === 'X,X,X') {
+    return true;
+  } else {
+    return false; 
+  }
     
-  }
-  }
+  };*/
   
   
-*/
+
 
 
 
