e31b2ad6d84de401faae3ef66b2bcb75b7737f6f
bellamysmith <bellamysmith1@gmail.com>
2014-08-26 09:03:05 -0700

    Added firebase and angular fire

--- TicTacToePlanets/js/app.js
+++ TicTacToePlanets/js/app.js
@@ -1,10 +1,21 @@
-var TicTac = angular.module("TicTac", []);
-// [] dependencies
-// a module contains models, controllers, views and services
+var TicTac = angular.module("TicTac", ["firebase"]);
 
-TicTac.controller('TicTacController', function ($scope) {
-  $scope.cells = ["", "", "", "", "", "", "", "", ""];
-  $scope.userToken = 1;
+
+TicTac.controller('TicTacController', ["$scope", "$firebase", function ($scope, $firebase) {
+  $scope.box = {cells:["", "", "", "", "", "", "", "", ""], userToken: 1}
+  //$scope.userToken = 1;
+  $scope.box.player1Counter = 0;
+  $scope.box.player2Counter = 0;
+
+
+ var ref = $firebase( new Firebase("https://unicorn-poo.firebaseio.com/data") );
+ ref.$bind($scope,'box');
+
+$scope.$watch('box', function(){
+  console.log('Hello!');
+});
+
+
 
 $scope.player1Name = function() {
   $scope.player1 = prompt('Player 1 Name: ', 'Player 1');
@@ -15,61 +26,65 @@ $scope.player2Name = function() {
 
 
 
+$scope.player1Name();
+$scope.player2Name();
 
 
-$scope.token = function(position) {
-    
 
-    if ($scope.userToken === 1 && $scope.cells[position] !== 'X' && $scope.cells[position] !== 'O') {
+$scope.token= function(position) {
+
+    if ($scope.box.userToken === 1 && $scope.box.cells[position] === "") {
+      $scope.box.cells[position] = $scope.currentSymbol(); 
       
-      this.cellToken = 'player1';
-      $scope.cells[position] = $scope.currentSymbol();
-
-    
-      
-      
-    } else if ($scope.userToken === 2 && $scope.cells[position] !== 'O' && $scope.cells[position] !== 'X') {
-      this.cellToken = 'player2';
-      $scope.cells[position] = $scope.currentSymbol();
+    } else if ($scope.box.userToken === 2 && $scope.box.cells[position] ==="") {
+      $scope.box.cells[position] = $scope.currentSymbol();
        
-      
-    } else if ($scope.userToken === 1) { 
-      $scope.userToken += 1;
-      $scope.cells[position] = $scope.cells[position];
-    } else if ($scope.userToken === 2) {
-      $scope.userToken -= 1;
-      $scope.cells[position] = $scope.cells[position];
-    }
-    console.log("token " + $scope.userToken)
+    } else if ($scope.box.userToken === 1) { 
+      $scope.box.userToken += 1;
+      $scope.box.cells[position] = $scope.box.cells[position];
 
+    } else if ($scope.box.userToken === 2) {
+      $scope.box.userToken -= 1;
+      $scope.box.cells[position] = $scope.box.cells[position];
+    }
+    
+    console.log("token " + $scope.box.userToken)
+    $scope.switch();
     $scope.checkForTie();
     $scope.checkForWin();
+
+    if ($scope.checkForWin() === true) {
+      if ($scope.box.userToken === 2) {
+      $scope.box.player1Counter += 1;
+    } else {
+      $scope.box.player2Counter += 1;
+    }
+    }
 
 //console.log($scope.checkForTie());
 //console.log('win: ' + $scope.checkForWin());
 
-    }
+    };
+  
 
   
 
 
   $scope.switch = function() {
 
-
-    if ($scope.userToken === 1) { 
-      $scope.userToken += 1;
-      
-  
+    if ($scope.box.userToken === 1) { 
+      $scope.box.userToken += 1;
+ 
     } else {
-      $scope.userToken -= 1;
-     
+      $scope.box.userToken -= 1;
       }
-    //console.log($scope.userToken)
     
 };
 
+
+
 $scope.currentSymbol = function() {
-    if ($scope.userToken === 1) {
+    if ($scope.box.userToken === 1) {
      return 'X';
     } else {
       return 'O';
@@ -77,14 +92,13 @@ $scope.currentSymbol = function() {
   };
 
 
-$scope.player1Name();
-$scope.player2Name();
+
 
   //tie if for every element in array, element != ''.
 
 $scope.checkForTie = function() {
-  for (i = 0; i < $scope.cells.length; i ++) {
-    if ($scope.cells.indexOf('') == -1) {
+  for (i = 0; i < $scope.box.cells.length; i ++) {
+    if ($scope.box.cells.indexOf('') == -1) {
     return true;
   } else {
     return false;
@@ -93,10 +107,11 @@ $scope.checkForTie = function() {
 };
 
 
+
 $scope.checkForWin = function() {
-  $scope.row1 = $scope.cells.slice(0,3);
-  $scope.row2 = $scope.cells.slice(3,6); 
-  $scope.row3 = $scope.cells.slice(6,9); 
+  $scope.row1 = $scope.box.cells.slice(0,3);
+  $scope.row2 = $scope.box.cells.slice(3,6); 
+  $scope.row3 = $scope.box.cells.slice(6,9); 
   $scope.column1 = [$scope.row1[0],$scope.row2[0], $scope.row3[0]];
   $scope.column2 = [$scope.row1[1], $scope.row2[1], $scope.row3[1]];
   $scope.column3 = [$scope.row1[2], $scope.row2[2], $scope.row3[2]];
@@ -106,8 +121,10 @@ $scope.checkForWin = function() {
   $scope.winningConditions = [$scope.row1.toString(), $scope.row2.toString(), $scope.row3.toString(), $scope.column1.toString(), $scope.column2.toString(), $scope.column3.toString(), $scope.diag1.toString(), $scope.diag2.toString()];
 
   for (i = 0; i < $scope.winningConditions.length; i ++) {
+    
     if ($scope.winningConditions[i] === 'X,X,X') {
       return true;
+
     } else if ($scope.winningConditions[i] === 'O,O,O') {
       return true;
     }
@@ -115,67 +132,21 @@ $scope.checkForWin = function() {
 
 }
 
+
 $scope.reset = function() {
-  $scope.cells = ["", "", "", "", "", "", "", "", ""];
-  $scope.userToken = 1;
+  $scope.box.cells = ["", "", "", "", "", "", "", "", ""];
+  $scope.box.userToken = 1;
+  
+
 }
+//check for end game and add to counter
 
 
 
 
+//$scope.player1Name();
+//$scope.player2Name();
 
-  /*if ($scope.row1 === 'X,X,X' || $scope.row2 === 'X,X,X' || $scope.row3 === 'X,X,X') {
-    return true;
-  } else if ($scope.row1 === 'O,O,O' || $scope.row2 === 'O,O,O' || $scope.row3 === 'O,O,O') {
-    return true;
-  } else if (for (i = 0; 1 < 9; i ++) {
-    $scope.col1[] = ($scope.cells[(i % 3 == 0)]) === 'X') {
-    return true;
-  }
-  } 
-}*/
-
-
-  /*if ($scope.row1 === 'X,X,X') {
-    return true;
-  } else {
-    return false; 
-  }
-    
-  };*/
-  
-  
-
-
-
-
-
-
-
-/*  if ($scope.cells.indexOf(['X','X','X'])) {
-  return true;
-} else {
-  return false;
-}
-}*/
-
-
-
-  //winner if array = [X,X,X]
-
-
-
-
-
-
-
-
-  
-    /*if ($scope.cells[position] == ''){
-      $scope.cells[position] = $scope.userToken;
-      console.log(position);
-    }*/
-
-});
+}]);
 
 
