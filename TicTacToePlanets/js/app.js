07a1ce85fa96543c167d4a1c38aeeb0362564f88
bellamysmith <bellamysmith1@gmail.com>
2014-08-28 22:08:22 -0700

    Updated with working 2 player firebase. Some styling changes.

--- TicTacToePlanets/js/app.js
+++ TicTacToePlanets/js/app.js
@@ -1,45 +1,62 @@
-var TicTac = angular.module("TicTac", ["firebase", "ngFx"]);
-
+var firebaseURL = "https://unicorn-poo.firebaseio.com/";
+var TicTac = angular.module("TicTac", ["firebase"]);
 
 TicTac.controller('TicTacController', ["$scope", "$firebase", function ($scope, $firebase) {
-  $scope.box = {
-      cells:["", "", "", "", "", "", "", "", ""], 
+    var syncObject = null;    // Firebase synced data
+    var unbindDB = null;      // function that unbinds the DB
+    var initialDBValues = {
+      //players: [],  // To remind me this is shared (but created later)
+      //guesses: [],  // To remind me this is shared (but created later)
+      cells:["","","","","","","","",""],
+      currPlayer: 0,
+      playerTurn: 0,
       gameInProgress: true,
-      userToken: 1,
       numberOfPlayers: 0
-    }
-  //$scope.userToken = 1;
-  $scope.box.player1Counter = 0;
-  $scope.box.player2Counter = 0;
+     };
 
- var firebaseURL = "https://unicorn-poo.firebaseio.com/data";
- var ref = $firebase( new Firebase("https://unicorn-poo.firebaseio.com/data") );
- ref.$bind($scope,'box').then(doneConnecting);
+    // Scope-wide variables
+    $scope.dbConnected  = false;  // true when connected to FireBase
+    $scope.dbConnecting = false;  // true when connecting to FireBase
+    $scope.playerNumber = 0;      // locally, which player this is
 
-$scope.$watch('box', function(){
-  console.log('Hello!');
-});
+    /**********************
+    * FIREBASE FUNCTIONS
+    **********************/
+
+    /* Called once when we are sure $scope.db is bound to Firebase */
+    function doneConnecting(unbindFunction) {
+        // If a game is not in progress, initialize Firebase database
+        if (!$scope.db.gameInProgress) {
+          $scope.db = initialDBValues;
+        }
+
+        // Set the local player number then increment the total # of players
+        $scope.playerNumber = $scope.db.numberOfPlayers;
+        $scope.db.numberOfPlayers += 1;
+        
+        
+        // Set player name by default to "Player <playerNumber>", if nothing entered
+        $scope.db.players = $scope.db.players || (new Array());
+        if ($scope.db.players.length < $scope.db.numberOfPlayers) {
+          var finalPlayerName = $scope.playerName || ("Player " + $scope.playerNumber);
+
+          $scope.db.players.push({'name': finalPlayerName});
+        }
+        
+        // We are done connecting!
+        $scope.dbConnecting = false;
+        $scope.dbConnected = true;
+
+        unbindDB = unbindFunction;
+
+    };
 
 
-
-$scope.player1Name = function() {
-  $scope.player1 = prompt('Player 1 Name: ', 'Player 1');
-}
-$scope.player2Name = function() {
-  $scope.player2 = prompt('Player 2 Name: ', 'Player 2');
-}
-
-
-$scope.player1Name();
-$scope.player2Name();
-/**************************************
-          Firebase
-***************************************/
-/* Connects to Firebase and binds the Firebase data to $scope.db */
+    /* Connects to Firebase and binds the Firebase data to $scope.db */
     $scope.connectDB = function() {
       $scope.dbConnecting = true; // indicate we are connecting to FB
 
-      var ref = new Firebase("https://unicorn-poo.firebaseio.com/data");
+      var ref = new Firebase(firebaseURL);
 
       // create an AngularFire reference to the data
       var sync = $firebase(ref);
@@ -53,120 +70,103 @@ $scope.player2Name();
         // Synchronize the object with a three-way data binding
         // Now, $scope.db is bound to Firebase.
         // Calls doneConnecting(<unbind function>) once $scope.db is bound to the database!
-        syncObject.$bindTo($scope, "box").then( doneConnecting );
+        syncObject.$bindTo($scope, "db").then( doneConnecting );
 
       });
     };
 
-function doneConnecting(unbindFunction) {
-        // If a game is not in progress, initialize Firebase database
-        if (!$scope.box.gameInProgress) {
-          $scope.box = $scope.box;
+
+    /* Unbinds from FireBase and sets dbConnected false */
+    function disconnectDB(ref) {
+        if (unbindDB) {
+          unbindDB();
         }
 
-        // Set the local player number then increment the total # of players
-        $scope.playerNumber = $scope.box.numberOfPlayers;
-        $scope.box.numberOfPlayers += 1;
-        console.log($scope.playerNumber);
-        
-        // Set player name by default to "Player <playerNumber>", if nothing entered
-        $scope.box.players = $scope.box.players || (new Array());
-        if ($scope.box.players.length < $scope.box.numberOfPlayers) {
-          var finalPlayerName = $scope.playerName || ("Player " + $scope.playerNumber);
-
-          $scope.box.players.push({'name': finalPlayerName});
+        if (syncObject) {
+          syncObject.$destroy();
         }
-        
-        // We are done connecting!
-        $scope.dbConnecting = false;
-        $scope.dbConnected = true;
 
-        unbindDB = unbindFunction;
+        $scope.dbConnected = false;
+    };
 
-    }
+    /* Ensure the game updated to not in progress then disconnect from the DB */
+    $scope.endGame = function() {
+      $scope.db.gameInProgress = false;
+      syncObject.$save().then( disconnectDB );
+    };
 
 
 
+
+
+/*************************
+*Tic Tac Toe Functions
+**************************/
 $scope.token= function(position) {
 
-    if ($scope.box.userToken === 1 && $scope.box.cells[position] === "") {
-      $scope.box.cells[position] = $scope.currentSymbol(); 
+    if ($scope.db.playerTurn === 0 && $scope.playerNumber === 0 && $scope.db.cells[position] === "") {
+      $scope.db.cells[position] = $scope.currentSymbol();
+      $scope.db.playerTurn ++; 
       
-    } else if ($scope.box.userToken === 2 && $scope.box.cells[position] ==="") {
-      $scope.box.cells[position] = $scope.currentSymbol();
+    } else if ($scope.db.playerTurn === 1 && $scope.playerNumber === 1 && $scope.db.cells[position] ==="") {
+      $scope.db.cells[position] = $scope.currentSymbol();
+      $scope.db.playerTurn --;
        
-    } else if ($scope.box.userToken === 1) { 
-      $scope.box.userToken += 1;
-      $scope.box.cells[position] = $scope.box.cells[position];
+    } else if ($scope.playerNumber === 0) { 
+      //$scope.playerNumber += 1;
+      $scope.db.cells[position] = $scope.db.cells[position];
 
-    } else if ($scope.box.userToken === 2) {
-      $scope.box.userToken -= 1;
-      $scope.box.cells[position] = $scope.box.cells[position];
+
+    } else if ($scope.playerNumber === 1) {
+      //$scope.playerNumber -= 1;
+      $scope.db.cells[position] = $scope.db.cells[position];
     }
     
-    console.log("token " + $scope.box.userToken)
-    $scope.switch();
+    console.log("token " + $scope.playerNumber)
     $scope.checkForTie();
     $scope.checkForWin();
 
     if ($scope.checkForWin() === true) {
-      if ($scope.box.userToken === 2) {
-      $scope.box.player1Counter += 1;
+      if ($scope.db.playerNumber === 1) {
+      $scope.player1Counter += 1;
     } else {
-      $scope.box.player2Counter += 1;
+      $scope.player2Counter += 1;
     }
     }
 
 console.log($scope.checkForTie());
-//console.log('win: ' + $scope.checkForWin());
+console.log('win: ' + $scope.checkForWin());
 
     };
-  
-
-  
-
-
-  $scope.switch = function() {
-
-    if ($scope.box.userToken === 1) { 
-      $scope.box.userToken += 1;
- 
-    } else {
-      $scope.box.userToken -= 1;
-      }
-    
-};
-
-
 
 $scope.currentSymbol = function() {
-    if ($scope.box.userToken === 1) {
+    if ($scope.playerNumber === 0) {
      return 'X';
     } else {
       return 'O';
     }
   };
 
-
-
-
-  //tie if for every element in array, element != ''.
+//tie if for every element in array, element != ''.
 
 $scope.checkForTie = function() {
-    if ($scope.box.cells.indexOf('') == -1) {
+  if ($scope.dbConnected == true){
+    if ($scope.db.cells.indexOf('') == -1) {
     return true;
   } else {
     return false;
   }
+}
   };
 
 
 
 
-$scope.checkForWin = function() {
-  $scope.row1 = $scope.box.cells.slice(0,3);
-  $scope.row2 = $scope.box.cells.slice(3,6); 
-  $scope.row3 = $scope.box.cells.slice(6,9); 
+$scope.checkForWin = function() { 
+  if ($scope.dbConnected == true){
+  $scope.row1 = $scope.db.cells.slice(0,3);
+  $scope.row2 = $scope.db.cells.slice(3,6); 
+  $scope.row3 = $scope.db.cells.slice(6,9); 
   $scope.column1 = [$scope.row1[0],$scope.row2[0], $scope.row3[0]];
   $scope.column2 = [$scope.row1[1], $scope.row2[1], $scope.row3[1]];
   $scope.column3 = [$scope.row1[2], $scope.row2[2], $scope.row3[2]];
@@ -186,36 +186,27 @@ $scope.checkForWin = function() {
   }
 
 }
+}
 
 
 $scope.reset = function() {
-  $scope.box.cells = ["", "", "", "", "", "", "", "", ""];
-  $scope.box.userToken = 1;
+  $scope.db.cells = ["", "", "", "", "", "", "", "", ""];
+  $scope.playerNumber = 0;
   
 
 }
-/* Connects to Firebase and binds the Firebase data to $scope.db 
-    $scope.connectDB = function() {
-      $scope.dbConnecting = true; // indicate we are connecting to FB
 
-      var ref = new Firebase(firebaseURL);
 
-      // create an AngularFire reference to the data
-      var sync = $firebase(ref);
 
-      // download the data into a local object
-      syncObject = sync.$asObject();
 
-      // wait until object is loaded
-      syncObject.$loaded().then(function() {
 
-        // Synchronize the object with a three-way data binding
-        // Now, $scope.db is bound to Firebase.
-        // Calls doneConnecting(<unbind function>) once $scope.db is bound to the database!
-        syncObject.$bindTo($scope, "box").then( doneConnecting );
 
-      });
-    };*/
+
+
+
+
+
+
+
+
 }]);
-
-
